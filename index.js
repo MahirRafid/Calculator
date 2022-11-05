c0171c76f52c2c65dc463cc7cdf81b2434d31aa1
Mahir Mosleh <mahirmosleh136@gmail.com>
2022-11-05 22:35:55 +0600

    Added functions for the basic operations

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ let nineBtn = document.querySelector(".nine");
 let zeroBtn = document.querySelector(".zero");
 let pointBtn = document.querySelector(".point");
 let equalBtn = document.querySelector(".equal");
-let adBtn = document.querySelector(".ad");
+let adBtn = document.querySelector(".plus");
 let subBtn = document.querySelector(".sub");
 let multiplyBtn = document.querySelector(".multiply");
 let divideBtn = document.querySelector(".divide");
@@ -37,3 +37,43 @@ function divide(a, b) {
 function operate(func, a, b) {
   return func(a, b);
 }
+
+let values = [];
+let sign = "";
+
+function pushValue(val) {
+  if (values.length == 2) values.shift();
+  values.push(val);
+}
+
+let ans = 0;
+
+function operate(sign, a, b) {
+  if (sign == "+") ans = a + b;
+  else if (sign == "-") ans = a - b;
+  else if (sign == "*") ans = a * b;
+  else ans = a / b;
+  console.log(ans);
+}
+
+function updateSign(val) {
+  sign = val;
+}
+
+oneBtn.addEventListener("click", (e) => pushValue(1));
+twoBtn.addEventListener("click", (e) => pushValue(2));
+threeBtn.addEventListener("click", (e) => pushValue(3));
+fourBtn.addEventListener("click", (e) => pushValue(4));
+fiveBtn.addEventListener("click", (e) => pushValue(5));
+sixBtn.addEventListener("click", (e) => pushValue(6));
+sevenBtn.addEventListener("click", (e) => pushValue(7));
+eightBtn.addEventListener("click", (e) => pushValue(8));
+nineBtn.addEventListener("click", (e) => pushValue(9));
+zeroBtn.addEventListener("click", (e) => pushValue(0));
+
+adBtn.addEventListener("click", (e) => updateSign("+"));
+subBtn.addEventListener("click", (e) => updateSign("-"));
+multiplyBtn.addEventListener("click", (e) => updateSign("*"));
+divideBtn.addEventListener("click", (e) => updateSign("/"));
+
+equalBtn.addEventListener("click", (e) => operate(sign, values[0], values[1]));
